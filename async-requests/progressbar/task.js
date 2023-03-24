6cea140de7c9f830a206884bb303bc701f120a6a
Test <m-mik@ro.ru>
2023-03-24 16:30:39 +0300

    Все задание выполнил

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -6,28 +6,19 @@ const input = document.querySelector("#file");
 const inputText = document.querySelector(".input__wrapper-desc");
 
 form.addEventListener("submit", (event) => {
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 1) {
-            progress.value = 0.25;
-        }
-        if (xhr.readyState == 2) {
-            progress.value = 0.5;
-        }
-        if (xhr.readyState == 3) {
-            progress.value = 0.7;
-        }
-        if (xhr.readyState == 4) {
-            progress.value = 1;
-        }
-        if (xhr.status >= 400) {
-            alert(`Упс.. Ошибка`);
-        }
-    };
-    xhr.open("POST", https);
-    xhr.send(input.files[0]);
-    event.preventDefault();
-    setInterval(() => {
-        inputText.textContent = "Файл успешно загружен";
-        progress.value = 0;
-    }, 1000);
+  xhr.upload.onprogress = function (event) {
+    progress.value = event.loaded;
+  };
+  xhr.onloadend = function () {
+    if (xhr.status >= 400) {
+      alert("Ошибка " + this.status);
+    }
+  };
+  xhr.open("POST", https);
+  xhr.send(input.files[0]);
+  event.preventDefault();
+  setInterval(() => {
+    inputText.textContent = "Файл успешно загружен";
+    progress.value = 0;
+  }, 1000);
 });
